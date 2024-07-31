baef34f738c4fafc6771f3fb59e9df3fdbd42890
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-08-01 00:09:56 +0530

    imagefixed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Landing() {
   return (
-    <div>
-      <img className='relative z-10 top-[224px] left-[42px] ' src="./Aspen.png" alt="" />
-      <img className=' w-full h-full top-0 sm:w-[375px] sm:h-[812px]  absolute z-[-1]' src="./Landingimg.png" alt="" />
-      <img src="./vacation.png" alt="" />
+    <div className="relative h-screen">
+      <img 
+        className="absolute z-10 top-[224px] left-[42px] sm:top-[100px] sm:left-[20px] md:top-[150px] md:left-[30px]" 
+        src="./Aspen.png" 
+        alt="Aspen" 
+      />
+      <img 
+        className="absolute z-[2] top-[553px] left-[32px] w-135 h-300 " 
+        src="./vacation.png" 
+        alt="Vacation" 
+      />
+      <img 
+        className="w-full h-full absolute top-0 z-[-1]" 
+        src="./Landingimg.png" 
+        alt="Landing Background" 
+      />
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
