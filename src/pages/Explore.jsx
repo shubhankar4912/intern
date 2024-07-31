ad9e5a5a802d746505f30a6d49fefbb88ebb8089
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-08-01 02:33:59 +0530

    bottomnavadded

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 function Explore() {
   return (
-    <div className='w-[375px] h-[812px]'>
+    <div className=' relative w-[375px] h-[812px]'>
         <div className=' flex justify-between w-[335px] h-[56px] mx-[20px] mt-[44px] '> 
             <div className='flex flex-col w-[103px] h-[56px] '>
                 <p className='w-[50px] text-lg'>Explore</p>
@@ -15,6 +16,22 @@ function Explore() {
             <div><img className='w-[16px] h-[16px] ml-[10px]' src="./Arrow - Down 2.png" alt="" /></div>
             </div>
         </div>
+        <div className=' mt-[60px]  mx-[20px]  '>
+        <div className="flex items-center bg-blue-50 rounded-full px-4 py-2 w-full max-w-md mx-auto">
+      <img src="./Search.png" className="text-gray-400 text-lg" alt="" />
+      <input 
+        type="text" 
+        placeholder="Find things to do" 
+        className="ml-2 bg-transparent outline-none text-gray-500 placeholder-gray-400 w-full"
+      />
+    </div>
+        </div>
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-white rounded-lg shadow-xl shadow-black flex justify-between px-5 w-[377px] h-[72px] pt-4">
+      <img className="w-[20x] h-[20px]" src="./nav/Home.png" alt="Home" />
+      <img className="w-[15px] h-[15px]" src="./nav/Ticket.png" alt="Ticket" />
+      <img className="w-[15px] h-[15px]" src="./nav/Heart.png" alt="Heart" />
+      <img className="w-[15px] h-[15px]" src="./nav/Profile.png" alt="Profile" />
+    </div>
       
     </div>
   )
