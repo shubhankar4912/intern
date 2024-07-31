7ddd350559da6432d7ca4bad6db8f586174fd787
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-07-31 23:24:11 +0530

    second

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,7 @@ function Landing() {
   return (
     <div>
       <img className='relative z-10 top-[224px] left-[42px] ' src="./Aspen.png" alt="" />
-      <img className='absolute z-[-1]' src="./Landingimg.png" alt="" />
+      <img className=' w-full h-screen absolute z-[-1]' src="./Landingimg.png" alt="" />
       <img src="./vacation.png" alt="" />
     </div>
   )
