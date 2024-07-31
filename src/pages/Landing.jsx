33cea3233c3e3936e174327cd68bd558632127bf
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-08-01 00:30:07 +0530

    Buttonadded

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,20 +4,26 @@ function Landing() {
   return (
     <div className="relative h-screen">
       <img 
-        className="absolute z-10 top-[224px] left-[42px] sm:top-[100px] sm:left-[20px] md:top-[150px] md:left-[30px]" 
+        className="absolute z-10  left-[57px] top-[93px] md:left-[30px]" 
         src="./Aspen.png" 
         alt="Aspen" 
       />
       <img 
-        className="absolute z-[2] top-[553px] left-[32px] w-135 h-300 " 
+        className="absolute z-[2] top-[553px] left-[32px] w-[300px] h-[135px]" 
         src="./vacation.png" 
         alt="Vacation" 
       />
       <img 
-        className="w-full h-full absolute top-0 z-[-1]" 
+        className="w-full h-full sm:w-full sm:h-full absolute top-0 z-[-1]" 
         src="./Landingimg.png" 
         alt="Landing Background" 
       />
+      <button 
+        type="button" 
+        className="absolute z-20 bottom-[20px] left-[33px] w-[311px] text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Explore
+      </button>
     </div>
   );
 }
