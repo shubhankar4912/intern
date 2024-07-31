a3abe709f0f0662804b1a490a277c770a22a911a
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-07-31 23:18:42 +0530

    first

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+      <img className='relative z-10 top-[224px] left-[42px] ' src="./Aspen.png" alt="" />
+      <img className='absolute z-[-1]' src="./Landingimg.png" alt="" />
+      <img src="./vacation.png" alt="" />
+    </div>
+  )
+}
+
+export default Landing
