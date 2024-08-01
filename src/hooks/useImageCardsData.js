697d6dbc5212497d21ef296ba8cbf2bf1a0fa2b9
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-08-01 15:49:04 +0530

    tajmahaladded

--- src/hooks/useImageCardsData.js
+++ src/hooks/useImageCardsData.js
@@ -4,13 +4,13 @@ const useImageCardsData = () => {
   const [cardsData, setCardsData] = useState([]);
 
   useEffect(() => {
-    // Simulating fetching data from an API or other sources
     const fetchData = async () => {
       const data = [
         { imageUrl: './card/Rectangle 992.png', title: 'Alley Palace', rating: 4.1 },
         { imageUrl: './card/Rectangle 994.png', title:'Coeurdes Alpes', rating: 4.5 },
         { imageUrl: './card/Rectangle 993.png', title: 'Beach Resort', rating: 4.7 },
-        // Add more card data objects as needed
+        { imageUrl: './card/rectangle.jpg', title: 'Taj Mahal', rating: 4.9 },
+     
       ];
       setCardsData(data);
     };
