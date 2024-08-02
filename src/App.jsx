92b0c46e15db459caa1a030ee16d2679ee5ba883
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-08-02 14:21:25 +0530

    vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename='/'>
+      <BrowserRouter >
         <Routes>
           <Route path='/' element={<Landing/>}/>
           <Route path='/explore' element={<Explore/>}/>
