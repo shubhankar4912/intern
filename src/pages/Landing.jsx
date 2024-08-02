edc69bba48c9f95051f81bca1acc39772d768074
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-08-02 14:42:45 +0530

    buttonchanged

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -25,7 +25,7 @@ function Landing() {
       />
       <button 
         type="button"  onClick={handleonclick}
-        className="fixed z-20 bottom-[20px] left-[60px] w-[311px] text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="fixed z-20 bottom-[20px] right-[31px] w-[311px] text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Explore
       </button>
